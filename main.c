fa7fac60bf9852dc676d28215a507a9c3c6302e4
Sean Hammell <sean.m.hammell@gmail.com>
2022-10-18 10:03:28 -0500

    Add negamax as opponent in game loop

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
-// #include "board.h"
 #include "solver.h"
+
+#define AI 0
+#define USER 1
 
 int main() {
     NCBoard position;
@@ -8,18 +10,43 @@ int main() {
     position.crosses = 0;
     position.nbmoves = 0;
 
+    int player;
+    printf("Would you like to go first? [y/n]: ");
+    player = getchar() == 'y' ? USER : AI;
+    while ((getchar()) != '\n');
+
     int move;
     while (!gameover(&position)) {
-        display(&position);
-        printf("Enter move: ");
-        scanf("%d", &move);
-        while ((getchar()) != '\n');
-        if (legal(&position, move)) {
-            play(&position, move);
-        } else if (move == -1) {
-            return 0;
+        if (player) {
+            display(&position);
+            printf("Enter move: ");
+            scanf("%d", &move);
+            while ((getchar()) != '\n');
+            if (legal(&position, move)) {
+                play(&position, move);
+                player = AI;
+            } else if (move == -1) {
+                return 0;
+            } else {
+                printf("Illegal move\n");
+            }
         } else {
-            printf("Illegal move\n");
+            int max = -999;
+            int score;
+
+            int moves[9];
+            int movelist_len = movegen(&position, moves);
+            for (int i = 0; i < movelist_len; ++i) {
+                play(&position, moves[i]);
+                score = -negamax(&position);
+                unmake(&position);
+                if (score > max) {
+                    max = score;
+                    move = moves[i];
+                }
+            }
+            play(&position, move);
+            player = USER;
         }
     }
 
