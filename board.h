74e85638fb3eeb428d372bf27d2d29dc05d76e48
Sean Hammell <sean.m.hammell@gmail.com>
2022-10-18 12:36:22 -0500

    Add description of NCBoard and related functions

--- board.h
+++ board.h
@@ -11,6 +11,29 @@ enum piece {
     CROSSES = 1
 };
 
+/*
+ * A struct storing a Noughts and Crosses position.
+ * 
+ * A binary bitboard representation for each marker
+ * type is used for the position.
+ * 
+ * Example of bit order to encode for a 3x3 board
+ * 
+ * 0 1 2
+ * 3 4 5
+ * 6 7 8
+ * . . .
+ * . . .
+ * . . .
+ * 
+ * NCBoard is stored as
+ * - an array of previous moves
+ * - an enum representing the side to play
+ * - the number of moves played
+ * - a bitboard "noughts" with a 1 on any nought
+ * - a bitboard "crosses" with a 1 on any cross
+ */
+
 typedef struct {
     int movelist[9];
     enum piece side;
@@ -19,12 +42,12 @@ typedef struct {
     unsigned short crosses;
 } NCBoard;
 
-int legal(const NCBoard *position, int move);
-void play(NCBoard *position, int move);
-void unmake(NCBoard *position);
-void playseq(NCBoard *position, char* seq);
-int alignment(unsigned short position);
-int gameover(NCBoard *position);
-void display(const NCBoard *position);
+int legal(const NCBoard *position, int move);   // Test if a move is legal
+void play(NCBoard *position, int move);         // Execute a move
+void unmake(NCBoard *position);                 // Undo the last move
+void playseq(NCBoard *position, char* seq);     // Play a sequence of moves
+int alignment(unsigned short position);         // Test for alignment
+int gameover(NCBoard *position);                // Test for engame conditions
+void display(const NCBoard *position);          // Print the position
 
 #endif
